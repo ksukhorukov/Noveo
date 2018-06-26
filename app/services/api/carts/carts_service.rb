c94783f9bc42706d5906e001ee6e49d04157d345
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2018-06-26 15:31:36 +0300

    to test

--- app/services/api/carts/carts_service.rb
+++ app/services/api/carts/carts_service.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
-class Api::CartsService
+class Api::Carts::CartsService
   attr_reader :params, :action
   attr_accessor :product, :errors
+
+  include Validations
 
   def initialize(params = {}, action)
     @params = params
@@ -15,18 +17,26 @@ class Api::CartsService
   end
 
   def add
-    return [format_errors, 400] unless valid_params?(:add)
+    return [response, 400] unless valid_params?(:add)
+    current_cart.add_to_cart(current_product)
+    [response, 200]
   end
 
   def delete
-    return [format_errors, 400] unless valid_params?(:delete)
+    return [response, 400] unless valid_params?(:delete)
+    current_cart.delete_from_cart(current_product)
+    [response, 200]
   end
 
   private
 
-  def cart
+  def current_cart
     Rails.cache.fetch(:cart, expires_in: 5.minutes) do
       Api::Cart.create
     end
+  end
+
+  def current_product
+    @product ||= Api::Product.find_by(id: params[:product_id])
   end
 end
